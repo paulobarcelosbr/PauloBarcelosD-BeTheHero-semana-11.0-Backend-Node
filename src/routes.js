29c3423e488c8f3df01e025c92641481231e375f
Paulo Barcelos <Paulo.barcelos@hotmail.com>
2020-03-28 11:57:55 -0300

    controller e rotas do profile.

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const OngController = require('./controllers/OngController');
 const IncidentController = require ('./controllers/IncidentController');
+const ProfileControlller = require ('./controllers/ProfileController');
+
 const  routes = express.Router();
 
 routes.get('/ongs',OngController.index);
 routes.post('/ongs', OngController.create )
+
+routes.get('/profile',ProfileControlller.index);
 
 routes.get('/incidents',IncidentController.index);
 routes.post('/incidents',IncidentController.create);
